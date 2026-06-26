8928ff59507ac44bf642795563cb2d5d33eebec4
javad bashiri <javad.bashiri1372@gmail.com>
2026-06-26 16:33:35 +0400

    add bug fix-2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
-import skills from "./components/skills";
+import Skills from "./components/Skills";
 import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
