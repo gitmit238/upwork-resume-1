1bd97b7abd40c1465e2a0916640f8138dcdf5025
javad bashiri <javad.bashiri1372@gmail.com>
2026-06-26 17:15:16 +0400

    add bug fix-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -7,6 +8,7 @@ import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+
 export default function App() {
   return (
     <div className="bg-[#030712] min-h-screen">
